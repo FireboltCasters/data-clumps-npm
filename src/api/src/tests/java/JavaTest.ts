e2e633e3e2b3fd822b763f3dbbbfa60101360e1a
Nils <nilsbaumgartner1994@gmail.com>
2023-03-30 13:44:57 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/api/src/tests/java/JavaTest.ts
+++ src/api/src/tests/java/JavaTest.ts
@@ -1,3 +1,5 @@
 test('Example test', async () => {
   expect("a").toBe("a");
 });
+
+export {} // In order to allow our outer react app to compile, we need to add an empty export statement to this file.
